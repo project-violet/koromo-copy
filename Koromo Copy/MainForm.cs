29157739b056102faa306c39eafb62068985e5bc
dc-koromo <koromo.software@gmail.com>
2018-08-31 20:39:28 +0900

    ui: add MetroModerUI

--- Koromo Copy/MainForm.cs
+++ Koromo Copy/MainForm.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Koromo_Copy
 {
-    public partial class MainForm : Form
+    public partial class MainForm : MetroFramework.Forms.MetroForm
     {
         public MainForm()
         {
