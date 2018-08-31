174f57bb3a9e0f0ef4131efa221dc45b95817627
dc-koromo <koromo.software@gmail.com>
2018-08-31 20:20:28 +0900

    kc: rename mainform

--- Koromo Copy/MainForm.Designer.cs
+++ Koromo Copy/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Koromo_Copy
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
--- Koromo Copy/MainForm.cs
+++ Koromo Copy/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Koromo_Copy
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
