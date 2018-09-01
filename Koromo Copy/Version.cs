92d9ac9a519a062332b1e80931c804e3febabfb1
dc-koromo <koromo.software@gmail.com>
2018-09-02 01:22:38 +0900

    update: Update first version

---
+++ Koromo Copy/Version.cs
@@ -0,0 +1,16 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+namespace Koromo_Copy
+{
+    public class Version
+    {
+        public const string Name = "Koromo Copy";
+        public const string Text = "0.1.0";
+    }
+}
