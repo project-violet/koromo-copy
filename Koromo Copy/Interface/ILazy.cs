197f4ebbed696c29aa82d60f79c9b685a6b5a648
dc-koromo <koromo.software@gmail.com>
2018-09-01 15:40:25 +0900

    kcp: add ILazy interface

---
+++ Koromo Copy/Interface/ILazy.cs
@@ -0,0 +1,23 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+
+namespace Koromo_Copy.Interface
+{
+    /// <summary>
+    /// Lazy구현을 쉽게 해주는 클래스입니다.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ILazy<T>
+        where T : new()
+    {
+        private static readonly Lazy<T> instance = new Lazy<T>(() => new T());
+        public static T Instance => instance.Value;
+    }
+}
