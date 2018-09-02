7596788c5c604c6f198414efb22d3f08fa49e2f7
dc-koromo <koromo.software@gmail.com>
2018-09-02 19:40:46 +0900

    grep: Add grep utility

---
+++ Koromo Copy/Console/GrepConsole.cs
@@ -0,0 +1,139 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Interface;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Koromo_Copy.Console
+{
+    /// <summary>
+    /// Grep 콘솔 옵션입니다.
+    /// </summary>
+    public class GrepConsoleOption : IConsoleOption
+    {
+        [CommandLine("--help", CommandType.OPTION, Default = true)]
+        public bool Help;
+
+        [CommandLine("-input", CommandType.ARGUMENTS, Pipe = true)]
+        public string[] Input;
+
+        [CommandLine("-p", CommandType.ARGUMENTS, DefaultArgument = true)]
+        public string[] Pattern;
+
+        [CommandLine("-i", CommandType.OPTION)]
+        public bool IgnoreCase;
+        [CommandLine("-r", CommandType.OPTION)]
+        public bool UsingRegex;
+        [CommandLine("-n", CommandType.OPTION)]
+        public bool ShowNumber;
+    }
+    
+    /// <summary>
+    /// 특정 구문을 찾기위한 Grep 콘솔 클래스입니다.
+    /// </summary>
+    public class GrepConsole : IConsole
+    {
+        /// <summary>
+        /// Grep 콘솔 리다이렉트
+        /// </summary>
+        static bool Redirect(string[] arguments, string contents)
+        {
+            arguments = CommandLineUtil.SplitCombinedOptions(arguments);
+            arguments = CommandLineUtil.InsertWeirdArguments<GrepConsoleOption>(arguments, contents != "", "-p");
+            GrepConsoleOption option = CommandLineParser<GrepConsoleOption>.Parse(arguments, contents != "", contents);
+
+            if (option.Input == null)
+            {
+                option.Input = new[] { contents };
+            }
+
+            if (option.Error)
+            {
+                Console.Instance.WriteLine(option.ErrorMessage);
+                if (option.HelpMessage != null)
+                    Console.Instance.WriteLine(option.HelpMessage);
+                return false;
+            }
+            else if (option.Help)
+            {
+                PrintHelp();
+            }
+            else if (option.Input == null)
+            {
+                Console.Instance.WriteErrorLine("Input is empty.");
+            }
+            else if (option.Pattern == null)
+            {
+                Console.Instance.WriteErrorLine("Pattern is empty.");
+            }
+            else
+            {
+                ProcessGrep(option.Input[0], option.Pattern[0], option.UsingRegex, option.ShowNumber, option.IgnoreCase);
+            }
+
+            return true;
+        }
+
+        bool IConsole.Redirect(string[] arguments, string contents)
+        {
+            return Redirect(arguments, contents);
+        }
+
+        static void PrintHelp()
+        {
+            Console.Instance.WriteLine(
+                "Grep Pipeline\r\n" +
+                "This command must be used with pipe."
+                );
+        }
+
+        static void ProcessGrep(string contents, string pattern, bool using_regex, bool show_number, bool ignore_case)
+        {
+            string[] lines = contents.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string match_string = pattern;
+                if (using_regex)
+                {
+                    var match = Regex.Match(lines[i], pattern, ignore_case ? RegexOptions.IgnoreCase : RegexOptions.None);
+                    if (match.Success == false) continue;
+                    match_string = match.Value;
+                }
+                else
+                {
+                    if (!ignore_case && !lines[i].Contains(pattern)) continue;
+                    if (ignore_case && !(lines[i]?.IndexOf(pattern, StringComparison.OrdinalIgnoreCase) >= 0)) continue;
+                }
+
+                int offset = lines[i].IndexOf(match_string);
+                string p1 = lines[i].Remove(offset);
+                string p2 = match_string;
+                string p3 = lines[i].Substring(offset + match_string.Length);
+
+                if (show_number)
+                {
+                    System.Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Instance.Write($"{i+1}");
+                    System.Console.ResetColor();
+                    Console.Instance.Write($":");
+                }
+
+                Console.Instance.Write(p1);
+                System.Console.ForegroundColor = ConsoleColor.Red;
+                Console.Instance.Write(p2);
+                System.Console.ResetColor();
+                Console.Instance.WriteLine(p3);
+            }
+        }
+    }
+}
