85a83a55cbe9f096fa44975fd53c50e96f8facd4
dc-koromo <koromo.software@gmail.com>
2018-09-03 19:56:01 +0900

    kcp: Add Selenium, Enum console prototype

---
+++ Koromo Copy/Console/Utility/EnumConsole.cs
@@ -0,0 +1,17 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+namespace Koromo_Copy.Console.Utility
+{
+    /// <summary>
+    /// 파일시스템 열거자입니다.
+    /// </summary>
+    public class EnumConsole
+    {
+    }
+}
---
+++ Koromo Copy/Console/Utility/SeleniumConsole.cs
@@ -0,0 +1,17 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+namespace Koromo_Copy.Console.Utility
+{
+    /// <summary>
+    /// 셀레니움을 컨트롤하는 콘솔 명령어 집합입니다.
+    /// </summary>
+    public class SeleniumConsole
+    {
+    }
+}
