927257a4e6167d21c9675113e4a5be034904d7cc
dc-koromo <koromo.software@gmail.com>
2018-09-04 15:09:31 +0900

    utility: Add FsEnumerator utility

---
+++ Koromo Copy/Utility/FsEnumerator.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace Koromo_Copy.Utility
+{
+    partial class FsEnumerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FsEnumerator));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lStatusMessage = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbIndexing = new System.Windows.Forms.ToolStripProgressBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lStatus,
+            this.lStatusMessage,
+            this.pbIndexing});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1274, 28);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lStatus
+            // 
+            this.lStatus.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(29, 23);
+            this.lStatus.Text = "Idle";
+            // 
+            // lStatusMessage
+            // 
+            this.lStatusMessage.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.lStatusMessage.Name = "lStatusMessage";
+            this.lStatusMessage.Size = new System.Drawing.Size(155, 23);
+            this.lStatusMessage.Text = "어떤 동작도 수행하지 않음";
+            this.lStatusMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pbIndexing
+            // 
+            this.pbIndexing.Name = "pbIndexing";
+            this.pbIndexing.Size = new System.Drawing.Size(300, 22);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 58);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1250, 480);
+            this.tabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1242, 452);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "모든 폴더";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(6, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1230, 440);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListView_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Index";
+            this.columnHeader1.Width = 64;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 886;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Size";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 98;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Rate";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader4.Width = 72;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Acc Rate";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader5.Width = 75;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1242, 452);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "탐색 폴더";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(6, 6);
+            this.listView2.MultiSelect = false;
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(1230, 440);
+            this.listView2.TabIndex = 8;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.DoubleClick += new System.EventHandler(this.listView2_DoubleClick);
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Index";
+            this.columnHeader6.Width = 64;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Path";
+            this.columnHeader7.Width = 853;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Size";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader8.Width = 98;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Rate by Folder";
+            this.columnHeader9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader9.Width = 101;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "Rate";
+            this.columnHeader10.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader10.Width = 75;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Total Size : ";
+            // 
+            // bStart
+            // 
+            this.bStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bStart.Location = new System.Drawing.Point(1152, 11);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(110, 26);
+            this.bStart.TabIndex = 14;
+            this.bStart.Text = "시작";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_ClickAsync);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Location = new System.Drawing.Point(12, 13);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(1134, 23);
+            this.tbPath.TabIndex = 13;
+            this.tbPath.Text = "C:\\";
+            // 
+            // FsEnumerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1274, 572);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(1290, 611);
+            this.Name = "FsEnumerator";
+            this.Text = "FileSystem Enumerator";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lStatusMessage;
+        private System.Windows.Forms.ToolStripProgressBar pbIndexing;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.TextBox tbPath;
+    }
+}
---
+++ Koromo Copy/Utility/FsEnumerator.cs
@@ -0,0 +1,170 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Fs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Koromo_Copy.Utility
+{
+    public partial class FsEnumerator : Form
+    {
+        public FsEnumerator()
+        {
+            InitializeComponent();
+        }
+        
+        private async void bStart_ClickAsync(object sender, EventArgs e)
+        {
+            tbPath.Enabled = false;
+            bStart.Enabled = false;
+            SetStatusMessage(StatusMessage.StartIndexing);
+            indexor = new FileIndexor();
+            await indexor.ListingDirectoryWithFilesAsync(tbPath.Text);
+            SetStatusMessage(StatusMessage.Enumerating);
+            await Task.Run(() => Processing());
+        }
+
+        #region 마무리
+
+        FileIndexor indexor;
+        
+        private void Processing()
+        {
+            int i = 1;
+            UInt64 total_size = indexor.GetTotalSize();
+            UInt64 acc_size = 0;
+            this.Post(() => label2.Text = $"{CapacityFormat(total_size)} ({total_size.ToString("#,0")} 바이트)");
+            List<ListViewItem> l = new List<ListViewItem>();
+            this.Post(() => pbIndexing.Maximum = indexor.Count);
+            foreach (var v in indexor.GetListSortWithNativeSize())
+            {
+                acc_size += v.Item2;
+                l.Add(new ListViewItem(new string[] { i++.ToString(), v.Item1,
+                        CapacityFormat(v.Item2), ((double)v.Item2 * 100 / total_size).ToString("0.0") + "%",
+                            ((double)acc_size * 100/ total_size).ToString("0.0") + "%"}));
+                this.Post(() => pbIndexing.Value++);
+            }
+            this.Post(() => listView1.Items.AddRange(l.ToArray()));
+
+            FileIndexorNode node = null;
+            i = 1;
+            node = indexor.GetPathNode(indexor.Node.Path);
+            foreach (FileIndexorNode n in node.GetListSortWithSize())
+            {
+                this.Post(() => listView2.Items.Add(new ListViewItem(new string[] { i++.ToString(), n.Path, CapacityFormat(n.Size),
+                        ((double)n.Size * 100/ node.Size).ToString("0.0") + "%",
+                        ((double)n.Size * 100/ indexor.GetTotalSize()).ToString("0.0") + "%"})));
+            }
+            this.Post(() => SetStatusMessage(StatusMessage.EndIndexing));
+        }
+
+        private string CapacityFormat(UInt64 i)
+        {
+            string capacity;
+            if (i / 1024 / 1024 / 1024 != 0) capacity = ((double)i / 1024 / 1024 / 1024).ToString("#,0.#") + " GB";
+            else if (i / 1024 / 1024 != 0) capacity = ((double)i / 1024 / 1024).ToString("#,0") + " MB";
+            else capacity = ((double)i / 1024).ToString("#,0") + " KB";
+            return capacity;
+        }
+
+        #endregion
+        
+        #region 폴더 클릭
+
+        string now_path;
+        Stack<string> ss = new Stack<string>();
+
+        private void listView2_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                string selected_path = listView2.SelectedItems[0].SubItems[1].Text;
+                bool c = listView2.SelectedItems[0].SubItems[0].Text == "..";
+                FileIndexorNode node = null;
+
+                listView2.Items.Clear();
+                if (c)
+                {
+                    string peek = ss.Pop();
+                    if (peek == null)
+                        peek = indexor.GetRootNode().Path;
+                    node = indexor.GetPathNode(peek);
+                    if (ss.Count != 0)
+                        listView2.Items.Add(new ListViewItem(new string[] { "..", peek }));
+                    now_path = peek;
+                }
+                else
+                {
+                    node = indexor.GetPathNode(selected_path);
+                    listView2.Items.Add(new ListViewItem(new string[] { "..", now_path }));
+                    ss.Push(now_path);
+                    now_path = selected_path;
+                }
+
+                int i = 1;
+                foreach (FileIndexorNode n in node.GetListSortWithSize())
+                {
+                    listView2.Items.Add(new ListViewItem(new string[] { i++.ToString(), n.Path, CapacityFormat(n.Size),
+                        ((double)n.Size * 100/ node.Size).ToString("0.0") + "%",
+                        ((double)n.Size * 100/ indexor.GetTotalSize()).ToString("0.0") + "%"}));
+                }
+            }
+        }
+
+        #endregion
+
+        private enum StatusMessage
+        {
+            StartIndexing,
+            EndIndexing,
+            Enumerating,
+            Idle,
+        }
+
+        private void SetStatusMessage(StatusMessage type)
+        {
+            switch (type)
+            {
+                case StatusMessage.Idle:
+                    lStatus.Text = "Idle";
+                    lStatusMessage.Text = "어떤 동작도 수행하지 않음";
+                    break;
+                case StatusMessage.StartIndexing:
+                    lStatus.Text = "Indexing..";
+                    lStatusMessage.Text = "폴더 인덱싱을 수행 중 입니다";
+                    pbIndexing.Style = ProgressBarStyle.Marquee;
+                    break;
+                case StatusMessage.Enumerating:
+                    lStatus.Text = "Enumerating..";
+                    lStatusMessage.Text = "나열 중 입니다";
+                    pbIndexing.Style = ProgressBarStyle.Blocks;
+                    break;
+                case StatusMessage.EndIndexing:
+                    lStatus.Text = "Success";
+                    lStatusMessage.Text = $"폴더 인덱싱이 성공적으로 수행되었습니다. ({indexor.Count}개의 폴더가 인덱싱됨)";
+                    pbIndexing.Style = ProgressBarStyle.Blocks;
+                    break;
+            }
+        }
+
+        private void ListView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListView lv = sender as ListView;
+            if (lv.SelectedItems.Count > 0)
+            {
+                Process.Start("explorer.exe", lv.SelectedItems[0].SubItems[1].Text);
+            }
+        }
+    }
+}
