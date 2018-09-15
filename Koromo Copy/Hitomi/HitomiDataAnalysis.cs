f3c3fef5bb7f781d6aa348287ea3c7da9751ed3f
dc-koromo <koromo.software@gmail.com>
2018-09-15 15:26:15 +0900

    hitomi: Add data analysis prototype

---
+++ Koromo Copy/Hitomi/HitomiDataAnalysis.cs
@@ -0,0 +1,17 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+namespace Koromo_Copy.Hitomi
+{
+    /// <summary>
+    /// 히토미 데이터에 관한 모든 정보를 분석합니다.
+    /// </summary>
+    public class HitomiDataAnalysis
+    {
+    }
+}
