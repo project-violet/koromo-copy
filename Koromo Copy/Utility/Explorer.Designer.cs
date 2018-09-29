0da24fd8bffe3f56a3d444fe135c2a4fc9f41494
dc-koromo <koromo.software@gmail.com>
2018-09-29 20:13:16 +0900

    utility: add explorer prototype

---
+++ Koromo Copy/Utility/Explorer.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Koromo_Copy.Utility
+{
+    partial class Explorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Explorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Explorer";
+            this.Text = "Explorer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
