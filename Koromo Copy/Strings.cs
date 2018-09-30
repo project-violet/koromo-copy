ae44eae0f32e09672674e9fbe31557fb4e97ba30
dc-koromo <koromo.software@gmail.com>
2018-09-30 15:05:57 +0900

    kcp: implements string compressions

--- Koromo Copy/Strings.cs
+++ Koromo Copy/Strings.cs
@@ -7,6 +7,8 @@
 ***/
 
 using System;
+using System.IO;
+using System.IO.Compression;
 using System.Text;
 
 namespace Koromo_Copy
@@ -132,6 +134,53 @@ namespace Koromo_Copy
             }
         }
 
+        #endregion
+
+        #region Compression
+
+        // https://stackoverflow.com/questions/7343465/compression-decompression-string-with-c-sharp
+        private static void CopyTo(Stream src, Stream dest)
+        {
+            byte[] bytes = new byte[4096];
+
+            int cnt;
+
+            while ((cnt = src.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                dest.Write(bytes, 0, cnt);
+            }
+        }
+
+        public static byte[] Zip(this string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(mso, CompressionMode.Compress))
+                {
+                    CopyTo(msi, gs);
+                }
+
+                return mso.ToArray();
+            }
+        }
+
+        public static string Unzip(this byte[] bytes)
+        {
+            using (var msi = new MemoryStream(bytes))
+            using (var mso = new MemoryStream())
+            {
+                using (var gs = new GZipStream(msi, CompressionMode.Decompress))
+                {
+                    CopyTo(gs, mso);
+                }
+
+                return Encoding.UTF8.GetString(mso.ToArray());
+            }
+        }
+        
         #endregion
     }
 }
