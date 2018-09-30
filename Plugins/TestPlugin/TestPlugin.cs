c869a72111f93329c9b0501d1729350f1f8f7c4f
dc-koromo <koromo.software@gmail.com>
2018-09-30 23:27:53 +0900

    plugin: test plugin added

---
+++ Plugins/TestPlugin/TestPlugin.cs
@@ -0,0 +1,24 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Hik.Sps;
+using Koromo_Copy.Plugin;
+
+namespace TestPlugin
+{
+    [PlugIn("Test", "0.1.0")]
+    public class TestPlugin : PlugIn<KoromoCopyPlugInBasedApplication>, INonePlugin
+    {
+        public KoromoCopyPlugInType Type => KoromoCopyPlugInType.None;
+        
+        public void Send(string user_input)
+        {
+            Application.ApplicationProxy.Send(this, $"Nothing happend! {Version} {user_input}", false);
+        }
+    }
+}
