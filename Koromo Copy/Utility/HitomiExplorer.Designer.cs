a735fdfe83c1e8908c7fd6cdaa877351b10f33f8
dc-koromo <koromo.software@gmail.com>
2018-10-03 14:55:13 +0900

    utility: hitomi explorer prototype

--- Koromo Copy/Utility/HitomiExplorer.Designer.cs
+++ Koromo Copy/Utility/HitomiExplorer.Designer.cs
@@ -29,21 +29,283 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HitomiExplorer));
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.PathTree = new Koromo_Copy.Controls.NativeTreeView();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.AvailableList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.bApply = new System.Windows.Forms.Button();
+            this.tbRule = new System.Windows.Forms.TextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.bOpen = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(702, 11);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(468, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Copyright (C) 2018. DCInside Programming Gallery Union. All Rights Reserved.";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 33);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1158, 527);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.splitContainer1);
+            this.tabPage1.Controls.Add(this.bApply);
+            this.tabPage1.Controls.Add(this.tbRule);
+            this.tabPage1.Controls.Add(this.metroLabel3);
+            this.tabPage1.Controls.Add(this.bOpen);
+            this.tabPage1.Controls.Add(this.tbPath);
+            this.tabPage1.Controls.Add(this.metroLabel2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1150, 499);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "메인";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(29, 86);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.PathTree);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel4);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.AvailableList);
+            this.splitContainer1.Panel2.Controls.Add(this.metroLabel5);
+            this.splitContainer1.Size = new System.Drawing.Size(1094, 383);
+            this.splitContainer1.SplitterDistance = 318;
+            this.splitContainer1.TabIndex = 16;
+            // 
+            // PathTree
+            // 
+            this.PathTree.Location = new System.Drawing.Point(3, 22);
+            this.PathTree.Name = "PathTree";
+            this.PathTree.ShowLines = false;
+            this.PathTree.Size = new System.Drawing.Size(312, 358);
+            this.PathTree.TabIndex = 15;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(94, 19);
+            this.metroLabel4.TabIndex = 14;
+            this.metroLabel4.Text = "파일 시스템 : ";
+            this.metroLabel4.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // AvailableList
+            // 
+            this.AvailableList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AvailableList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.AvailableList.FullRowSelect = true;
+            this.AvailableList.GridLines = true;
+            this.AvailableList.Location = new System.Drawing.Point(3, 22);
+            this.AvailableList.Name = "AvailableList";
+            this.AvailableList.Size = new System.Drawing.Size(768, 358);
+            this.AvailableList.TabIndex = 13;
+            this.AvailableList.UseCompatibleStateImageBehavior = false;
+            this.AvailableList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "인덱스";
+            this.columnHeader1.Width = 78;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "파일경로";
+            this.columnHeader2.Width = 446;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "추출된 이름";
+            this.columnHeader3.Width = 137;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "가용성";
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.AutoSize = true;
+            this.metroLabel5.Location = new System.Drawing.Point(3, 0);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(122, 19);
+            this.metroLabel5.TabIndex = 15;
+            this.metroLabel5.Text = "가용성있는 항목 : ";
+            this.metroLabel5.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // bApply
+            // 
+            this.bApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bApply.Location = new System.Drawing.Point(1049, 51);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(74, 24);
+            this.bApply.TabIndex = 12;
+            this.bApply.Text = "적용";
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // tbRule
+            // 
+            this.tbRule.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRule.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRule.Location = new System.Drawing.Point(83, 52);
+            this.tbRule.Name = "tbRule";
+            this.tbRule.Size = new System.Drawing.Size(960, 22);
+            this.tbRule.TabIndex = 10;
+            this.tbRule.Text = "^\\[(\\d+)\\], ^\\[.*?\\((\\d+)\\).*?\\], \\((\\d+)\\)$";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(29, 54);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(48, 19);
+            this.metroLabel3.TabIndex = 9;
+            this.metroLabel3.Text = "규칙 : ";
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // bOpen
+            // 
+            this.bOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bOpen.Location = new System.Drawing.Point(1049, 22);
+            this.bOpen.Name = "bOpen";
+            this.bOpen.Size = new System.Drawing.Size(74, 24);
+            this.bOpen.TabIndex = 8;
+            this.bOpen.Text = "열기";
+            this.bOpen.UseVisualStyleBackColor = true;
+            this.bOpen.Click += new System.EventHandler(this.bOpen_ClickAsync);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPath.Font = new System.Drawing.Font("Consolas", 9F);
+            this.tbPath.Location = new System.Drawing.Point(83, 23);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(960, 22);
+            this.tbPath.TabIndex = 7;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(29, 25);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(48, 19);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "경로 : ";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1150, 499);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "작업";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // HitomiExplorer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(831, 394);
+            this.ClientSize = new System.Drawing.Size(1182, 568);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.metroLabel1);
+            this.Font = new System.Drawing.Font("맑은 고딕", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "HitomiExplorer";
             this.Text = "Hitomi Explorer";
+            this.Load += new System.EventHandler(this.HitomiExplorer_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private System.Windows.Forms.ListView AvailableList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.TextBox tbRule;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private System.Windows.Forms.Button bOpen;
+        private System.Windows.Forms.TextBox tbPath;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private Controls.NativeTreeView PathTree;
     }
 }
--- Koromo Copy/Utility/HitomiExplorer.cs
+++ Koromo Copy/Utility/HitomiExplorer.cs
@@ -1,20 +1,222 @@
-﻿using System;
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using Etier.IconHelper;
+using Koromo_Copy.Controls;
+using Koromo_Copy.Fs;
+using Koromo_Copy.Fs.FileIcon;
+using Koromo_Copy.Hitomi;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace Koromo_Copy.Utility
 {
     public partial class HitomiExplorer : Form
     {
+        ImageList smallImageList = new ImageList();
+        ImageList largeImageList = new ImageList();
+        IconListManager iconListManager;
+        FileIndexor indexor;
+
         public HitomiExplorer()
         {
             InitializeComponent();
+
+            smallImageList.ColorDepth = ColorDepth.Depth32Bit;
+            largeImageList.ColorDepth = ColorDepth.Depth32Bit;
+
+            smallImageList.ImageSize = new System.Drawing.Size(16, 16);
+            largeImageList.ImageSize = new System.Drawing.Size(32, 32);
+
+            smallImageList.Images.Add(FolderIcon.GetFolderIcon(
+                FolderIcon.IconSize.Small,
+                FolderIcon.FolderType.Closed));
+
+            iconListManager = new IconListManager(smallImageList, largeImageList);
+
+            PathTree.ImageList = smallImageList;
         }
+
+        private void HitomiExplorer_Load(object sender, EventArgs e)
+        {
+            ColumnSorter.InitListView(AvailableList);
+
+            string[] splits = Settings.Instance.Hitomi.Path.Split('\\');
+            string path = "";
+            for (int i = 0; i < splits.Length; i++)
+            {
+                if (!(splits[i].Contains("{") && splits[i].Contains("}")))
+                    path += splits[i] + "\\";
+                else
+                    break;
+            }
+            tbPath.Text = path;
+        }
+
+        #region 파일 시스템 리스팅
+
+        private async void bOpen_ClickAsync(object sender, EventArgs e)
+        {
+            indexor = null;
+            indexor = new FileIndexor();
+            PathTree.Nodes.Clear();
+            AvailableList.Items.Clear();
+
+            Monitor.Instance.Push($"[Hitomi Explorer] Open directory '{tbPath.Text}'");
+            await indexor.ListingDirectoryAsync(tbPath.Text);
+            Monitor.Instance.Push($"[Hitomi Explorer] Complete open! DirCount={indexor.Count}");
+
+            listing(indexor);
+        }
+
+        private void listing(FileIndexor fi)
+        {
+            FileIndexorNode node = fi.GetRootNode();
+            foreach (FileIndexorNode n in node.Nodes)
+            {
+                make_node(PathTree.Nodes, Path.GetFileName(n.Path.Remove(n.Path.Length - 1)));
+                make_tree(n, PathTree.Nodes[PathTree.Nodes.Count - 1]);
+            }
+            foreach (FileInfo f in new DirectoryInfo(node.Path).GetFiles())
+                make_node(PathTree.Nodes, f.Name);
+        }
+
+        private void make_tree(FileIndexorNode fn, TreeNode tn)
+        {
+            foreach (FileIndexorNode n in fn.Nodes)
+            {
+                make_node(tn.Nodes, Path.GetFileName(n.Path.Remove(n.Path.Length - 1)));
+                make_tree(n, tn.Nodes[tn.Nodes.Count - 1]);
+            }
+            foreach (FileInfo f in new DirectoryInfo(fn.Path).GetFiles())
+                make_node(tn.Nodes, f.Name);
+        }
+
+        private void make_node(TreeNodeCollection tnc, string path)
+        {
+            TreeNode tn = new TreeNode(path);
+            tnc.Add(tn);
+            string fullpath = Path.Combine(tbPath.Text, tn.FullPath);
+            if (File.Exists(fullpath))
+            {
+                int index = iconListManager.AddFileIcon(fullpath);
+                tn.ImageIndex = index;
+                tn.SelectedImageIndex = index;
+            }
+            else
+            {
+                tn.ImageIndex = 0;
+            }
+        }
+
+        #endregion
+
+        #region 규칙 적용 및 추출
+
+        List<Tuple<string, string, HitomiMetadata?>> metadatas = new List<Tuple<string, string, HitomiMetadata?>>();
+        List<KeyValuePair<string, int>> artist_rank;
+        int visit_count = 0;
+        int available_count = 0;
+
+        bool show_unavailables = true;
+        bool deal_group_with_artist = false;
+        bool search_subfiles_whenever = false;
+        bool deal_artists_with_parent_folder = true;
+
+        private void bApply_Click(object sender, EventArgs e)
+        {
+            var regexs = tbRule.Text.Split(',').ToList().Select(x => new Regex(x.Trim())).ToList();
+            metadatas.Clear();
+            visit_count = 0;
+            available_count = 0;
+            foreach (var node in PathTree.Nodes.OfType<TreeNode>())
+                RecursiveVisit(node, regexs);
+            Monitor.Instance.Push($"[Hitomi Explorer] Apply! visit_count={visit_count}, available_count={available_count}");
+
+            List<ListViewItem> lvil = new List<ListViewItem>();
+            for (int i = 0; i < metadatas.Count; i++)
+            {
+                if (show_unavailables == false && !metadatas[i].Item3.HasValue) continue;
+                lvil.Add(new ListViewItem(new string[]
+                {
+                    (i+1).ToString(),
+                    metadatas[i].Item1,
+                    metadatas[i].Item2,
+                    metadatas[i].Item3.HasValue.ToString()
+                }));
+            }
+            AvailableList.Items.Clear();
+            AvailableList.Items.AddRange(lvil.ToArray());
+
+            //////////////////////////////////////////////////
+
+            Dictionary<string, int> artist_count = new Dictionary<string, int>();
+            foreach (var md in metadatas)
+            {
+                if (!deal_artists_with_parent_folder)
+                {
+                    if (md.Item3.HasValue && md.Item3.Value.Artists != null)
+                        foreach (var artist in md.Item3.Value.Artists)
+                            if (artist_count.ContainsKey(artist))
+                                artist_count[artist] += 1;
+                            else
+                                artist_count.Add(artist, 1);
+                    if (deal_group_with_artist && md.Item3.HasValue && md.Item3.Value.Groups != null)
+                        foreach (var group in md.Item3.Value.Groups)
+                        {
+                            string tmp = "group:" + group;
+                            if (artist_count.ContainsKey(tmp))
+                                artist_count[tmp] += 1;
+                            else
+                                artist_count.Add(tmp, 1);
+                        }
+                }
+                else
+                {
+                    var split = md.Item1.Split('\\');
+                    string parent_folder = split.Length >= 2 ? split[split.Length - 2] : "";
+                    if (artist_count.ContainsKey(parent_folder))
+                        artist_count[parent_folder] += 1;
+                    else
+                        artist_count.Add(parent_folder, 1);
+                }
+            }
+
+            artist_rank = artist_count.ToList();
+            artist_rank.Sort((a, b) => b.Value.CompareTo(a.Value));
+        }
+
+        private void RecursiveVisit(TreeNode node, List<Regex> regex)
+        {
+            string match = "";
+            if (regex.Any(x => {
+                if (!x.Match(Path.GetFileNameWithoutExtension(node.Text)).Success) return false;
+                match = x.Match(Path.GetFileNameWithoutExtension(node.Text)).Groups[1].Value;
+                return true;
+            }))
+            {
+                metadatas.Add(new Tuple<string, string, HitomiMetadata?>(node.FullPath, match, HitomiDataAnalysis.GetMetadataFromMagic(match)));
+                available_count += 1;
+                if (!search_subfiles_whenever)
+                    return;
+            }
+
+            visit_count += 1;
+
+            foreach (var cnode in node.Nodes.OfType<TreeNode>())
+                RecursiveVisit(cnode, regex);
+        }
+
+        #endregion
+
     }
 }
