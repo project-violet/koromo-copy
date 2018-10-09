9e76a9291fd5717fe1e8413e73997908f1833663
dc-koromo <koromo.software@gmail.com>
2018-10-09 22:13:59 +0900

    kcp: add format prototype

---
+++ Koromo Copy/Format.cs
@@ -0,0 +1,18 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+
+namespace Koromo_Copy
+{
+    public class Format
+    {
+        public static string GetFileDateTime(DateTime dt) =>
+            dt.ToString("yyyy-mm-dd");
+    }
+}
