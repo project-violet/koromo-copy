6f5ad9e07c9098a91a647eddd1957a123ebeffd0
dc-koromo <koromo.software@gmail.com>
2018-10-09 22:07:08 +0900

    kcp: add statistics prototype

---
+++ Koromo Copy/Statistics.cs
@@ -0,0 +1,16 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Interface;
+
+namespace Koromo_Copy
+{
+    public class Statistics : ILazy<Statistics>
+    {
+    }
+}
