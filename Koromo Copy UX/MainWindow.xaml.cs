3d5938896b9a011ab691eed47db0f52872b6c559
dc-koromo <koromo.software@gmail.com>
2018-10-27 14:38:54 +0900

    ux: apply modern chorome theme

--- Koromo Copy UX/MainWindow.xaml.cs
+++ Koromo Copy UX/MainWindow.xaml.cs
@@ -1,6 +1,16 @@
-﻿using System;
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+   
+***/
+
+using ModernChrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +18,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -18,11 +29,17 @@ namespace Koromo_Copy_UX
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ModernWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        //private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    if (e.ChangedButton == MouseButton.Left)
+        //        this.DragMove();
+        //}
     }
 }
