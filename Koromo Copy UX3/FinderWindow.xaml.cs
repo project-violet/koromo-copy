63102faf29ad6657dbca5467aa11564f58faf919
dc-koromo <koromo.software@gmail.com>
2018-11-08 12:03:40 +0900

    ux: Implements AutoComplete on FinderWindow

--- Koromo Copy UX3/FinderWindow.xaml.cs
+++ Koromo Copy UX3/FinderWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Koromo_Copy_UX3.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -22,6 +25,59 @@ namespace Koromo_Copy_UX3
         public FinderWindow()
         {
             InitializeComponent();
+
+            Loaded += FinderWindow_Loaded;
         }
+
+        private void FinderWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            logic = new AutoCompleteLogic(SearchText, AutoComplete, AutoCompleteList);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tag = (sender as Button).Tag.ToString();
+
+            if (tag == "Search")
+            {
+                //AppendAsync(SearchText.Text);
+            }
+        }
+        
+        #region Search Helper
+        AutoCompleteLogic logic;
+
+        private void SearchText_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return && !logic.skip_enter)
+            {
+                ButtonAutomationPeer peer = new ButtonAutomationPeer(SearchButton);
+                IInvokeProvider invokeProv = peer.GetPattern(PatternInterface.Invoke) as IInvokeProvider;
+                invokeProv.Invoke();
+            }
+            logic.skip_enter = false;
+        }
+
+        private void SearchText_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            logic.SearchText_PreviewKeyDown(sender, e);
+        }
+
+        private void SearchText_KeyUp(object sender, KeyEventArgs e)
+        {
+            logic.SearchText_KeyUp(sender, e);
+        }
+
+        private void AutoCompleteList_KeyUp(object sender, KeyEventArgs e)
+        {
+            logic.AutoCompleteList_KeyUp(sender, e);
+        }
+
+        private void AutoCompleteList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            logic.AutoCompleteList_MouseDoubleClick(sender, e);
+        }
+        #endregion
+
     }
 }
