d55dcec0b7d0c6abb63b403b0b9d442722528e76
dc-koromo <koromo.software@gmail.com>
2018-11-13 19:46:53 +0900

    ux: Implements SettingViewHitomi and ViewModel

---
+++ Koromo Copy UX3/Domain/SettingHitomiViewModel.cs
@@ -0,0 +1,149 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy_UX3.Domain
+{
+    public class SettingHitomiViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        public bool AutoSync
+        {
+            get { return Settings.Instance.Hitomi.AutoSync; }
+            set
+            {
+                if (Settings.Instance.Hitomi.AutoSync == value) return;
+                Settings.Instance.Hitomi.AutoSync = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingOptimization
+        {
+            get { return Settings.Instance.Hitomi.UsingOptimization; }
+            set
+            {
+                if (Settings.Instance.Hitomi.UsingOptimization == value) return;
+                Settings.Instance.Hitomi.UsingOptimization = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingFuzzy
+        {
+            get { return Settings.Instance.Hitomi.UsingFuzzy; }
+            set
+            {
+                if (Settings.Instance.Hitomi.UsingFuzzy == value) return;
+                Settings.Instance.Hitomi.UsingFuzzy = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingAdvancedSearch
+        {
+            get { return Settings.Instance.Hitomi.UsingAdvancedSearch; }
+            set
+            {
+                if (Settings.Instance.Hitomi.UsingAdvancedSearch == value) return;
+                Settings.Instance.Hitomi.UsingAdvancedSearch = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingOnlyFMTagsOnAnalysis
+        {
+            get { return Settings.Instance.HitomiAnalysis.UsingOnlyFMTagsOnAnalysis; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.UsingOnlyFMTagsOnAnalysis == value) return;
+                Settings.Instance.HitomiAnalysis.UsingOnlyFMTagsOnAnalysis = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingXiAanlysis
+        {
+            get { return Settings.Instance.HitomiAnalysis.UsingXiAanlysis; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.UsingXiAanlysis == value) return;
+                Settings.Instance.HitomiAnalysis.UsingXiAanlysis = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingRMSAanlysis
+        {
+            get { return Settings.Instance.HitomiAnalysis.UsingRMSAanlysis; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.UsingRMSAanlysis == value) return;
+                Settings.Instance.HitomiAnalysis.UsingRMSAanlysis = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool UsingCosineAnalysis
+        {
+            get { return Settings.Instance.HitomiAnalysis.UsingCosineAnalysis; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.UsingCosineAnalysis == value) return;
+                Settings.Instance.HitomiAnalysis.UsingCosineAnalysis = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool RecommendNMultipleWithLength
+        {
+            get { return Settings.Instance.HitomiAnalysis.RecommendNMultipleWithLength; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.RecommendNMultipleWithLength == value) return;
+                Settings.Instance.HitomiAnalysis.RecommendNMultipleWithLength = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+        
+        public bool RecommendLanguageALL
+        {
+            get { return Settings.Instance.HitomiAnalysis.RecommendLanguageALL; }
+            set
+            {
+                if (Settings.Instance.HitomiAnalysis.RecommendLanguageALL == value) return;
+                Settings.Instance.HitomiAnalysis.RecommendLanguageALL = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Koromo Copy UX3/SettingViewHitomi.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Koromo_Copy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3
+{
+    /// <summary>
+    /// SettingViewHitomi.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingViewHitomi : UserControl
+    {
+        public SettingViewHitomi()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var value = (sender as TextBox).Text;
+
+            if (string.IsNullOrWhiteSpace((value ?? "").ToString()))
+            {
+                Error.Text = "경로는 비어있으면 안됩니다.";
+                Error.Visibility = Visibility.Visible;
+                return;
+            }
+
+            var ss = "";
+            foreach (var s in value.ToString().Split('\\'))
+                if (s.StartsWith("{")) break;
+                else ss += s + "\\";
+            if (!Directory.Exists(ss))
+            {
+                Error.Text = "유효하지 않은 경로입니다.";
+                Error.Visibility = Visibility.Visible;
+                return;
+            }
+
+            if (!(value.ToString().ToLower().Contains("{id}") || value.ToString().ToLower().Contains("{title}")))
+            {
+                Error.Text = "{Id} 또는 {Title}를 반드시 하나이상 포함해야합니다.";
+                Error.Visibility = Visibility.Visible;
+                return;
+            }
+
+            var valid_tokens = new string[]
+            {
+                "{title}",
+                "{artists}",
+                "{id}",
+                "{type}",
+                "{date}",
+                "{series}",
+                "{search}"
+            };
+            var regex = Regex.Matches(value.ToString(), @"(\{.*?\})");
+            if (!regex.OfType<Match>().All(x => valid_tokens.Contains(x.Value.ToLower())))
+            {
+                Error.Text = "유효하지 않은 토큰이 있습니다. " + string.Join(", ", regex.OfType<Match>().Where(x => !valid_tokens.Contains(x.Value.ToLower())));
+                Error.Visibility = Visibility.Visible;
+                return;
+            }
+
+            Error.Visibility = Visibility.Collapsed;
+            Settings.Instance.Hitomi.Path = value;
+            Settings.Instance.Save();
+        }
+    }
+}
