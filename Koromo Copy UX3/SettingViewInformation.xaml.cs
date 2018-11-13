d0418383434a1539c306d68037936ffd9b405446
dc-koromo <koromo.software@gmail.com>
2018-11-13 19:47:24 +0900

    ux: Implements SettingViewInformation

---
+++ Koromo Copy UX3/SettingViewInformation.xaml.cs
@@ -0,0 +1,45 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3
+{
+    /// <summary>
+    /// SettingViewInformation.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SettingViewInformation : UserControl
+    {
+        public SettingViewInformation()
+        {
+            InitializeComponent();
+
+            VersionText.Text += Koromo_Copy.Version.Text;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
