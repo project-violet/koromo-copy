a63bd50a15efd9215b1a3abf9aedb2388848de0b
dc-koromo <koromo.software@gmail.com>
2018-11-17 09:40:08 +0900

    ux: implements setting downloader view model

---
+++ Koromo Copy UX3/Domain/SettingDownloaderViewModel.cs
@@ -0,0 +1,44 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy;
+using Koromo_Copy.Net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy_UX3.Domain
+{
+    public class SettingDownloaderViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public int Thread
+        {
+            get { return Settings.Instance.Model.Thread; }
+            set
+            {
+                if (Settings.Instance.Model.Thread == value) return;
+                Settings.Instance.Model.Thread = value;
+                DownloadGroup.Instance.Queue.Capacity = value;
+                Settings.Instance.Save();
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
