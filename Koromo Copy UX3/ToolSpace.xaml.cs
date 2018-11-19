e0ddf47fc75f1eed0f693d31731f7d32e2086c79
dc-koromo <koromo.software@gmail.com>
2018-11-19 10:17:54 +0900

    ux: Add ToolSpace prototype

---
+++ Koromo Copy UX3/ToolSpace.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3
+{
+    /// <summary>
+    /// ToolSpace.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ToolSpace : UserControl
+    {
+        public ToolSpace()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var item = SettingsTree.SelectedItem;
+            if (item is TreeViewItem tvi)
+            {
+                if (tvi.DataContext != null)
+                {
+                    ContentControl.Content = tvi.DataContext;
+                }
+            }
+        }
+    }
+}
