2626124d144bb39cb9f3bec94ff5dafe09d07590
dc-koromo <koromo.software@gmail.com>
2018-11-25 13:48:41 +0900

    ux: implements trends

--- Koromo Copy UX3/Tools/Statistics.xaml.cs
+++ Koromo Copy UX3/Tools/Statistics.xaml.cs
@@ -97,6 +97,44 @@ namespace Koromo_Copy_UX3.Tools
             trends.ToList().ForEach(lang => Trend.Items.Add(new ComboBoxItem { Content = lang }));
         }
 
+        private void LoadTrend(string title)
+        {
+            YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
+            SeriesCollection.Clear();
+            SeriesCollection.Add(new LineSeries
+            {
+                Title = title,
+                Values = new ChartValues<ScatterPoint>(
+                        HitomiAnalysisTrend.Instance.samples[0].points.Select(x => new ScatterPoint(x.X, x.Y))),
+                Fill = Brushes.Transparent,
+                Stroke = Brushes.Pink,
+                LineSmoothness = 1,
+                PointGeometrySize = 0
+            });
+        }
+
+        private void LoadTrend2()
+        {
+            YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
+            SeriesCollection.Clear();
+            SeriesCollection.AddRange(
+                HitomiAnalysisTrend.Instance.samples.Select(x =>
+                {
+                    Random rm = new Random(x.name.GetHashCode());
+                    return new LineSeries
+                    {
+                        Title = x.name,
+                        Values = new ChartValues<ScatterPoint>(
+                            x.points.Select(y => new ScatterPoint(y.X, y.Y))),
+                        Fill = Brushes.Transparent,
+                        Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
+                        LineSmoothness = 1,
+                        PointGeometrySize = 0
+                    };
+                })
+                .ToArray());
+        }
+
         private void Trend_DropDownClosed(object sender, EventArgs e)
         {
             if (loaded)
@@ -108,36 +146,14 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 0;
                         AxisY.Title = "작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.Add(new LineSeries
-                        {
-                            Title = "작품 수",
-                            Values = new ChartValues<ScatterPoint>(
-                                    HitomiAnalysisTrend.Instance.samples[0].points.Select(x => new ScatterPoint(x.X, x.Y))),
-                            Fill = Brushes.Transparent,
-                            Stroke = Brushes.Pink,
-                            LineSmoothness = 1,
-                            PointGeometrySize = 0
-                        });
+                        LoadTrend("작품 수");
                         break;
 
                     case "작품 업로드 누적 폭":
                         HitomiAnalysisTrend.Instance.UpdataGalleryIncrements();
                         AxisX.Title = "아이디 간격";
                         AxisY.Title = "누적 작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.Add(new LineSeries
-                        {
-                            Title = "누적 작품 수",
-                            Values = new ChartValues<ScatterPoint>(
-                                    HitomiAnalysisTrend.Instance.samples[0].points.Select(x => new ScatterPoint(x.X, x.Y))),
-                            Fill = Brushes.Transparent,
-                            Stroke = Brushes.Pink,
-                            LineSmoothness = 1,
-                            PointGeometrySize = 0
-                        });
+                        LoadTrend("누적 작품 수");
                         break;
 
                     case "태그 누적":
@@ -145,24 +161,7 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 0;
                         AxisY.Title = "누적 작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.AddRange(
-                            HitomiAnalysisTrend.Instance.samples.Select(x =>
-                            {
-                                Random rm = new Random(x.name.GetHashCode());
-                                return new LineSeries
-                                {
-                                    Title = x.name,
-                                    Values = new ChartValues<ScatterPoint>(
-                                        x.points.Select(y => new ScatterPoint(y.X, y.Y))),
-                                    Fill = Brushes.Transparent,
-                                    Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
-                                    LineSmoothness = 1,
-                                    PointGeometrySize = 0
-                                };
-                            })
-                            .ToArray());
+                        LoadTrend2();
                         break;
 
                     case "태그 누적 - 한국어":
@@ -170,24 +169,7 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 0;
                         AxisY.Title = "누적 작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.AddRange(
-                            HitomiAnalysisTrend.Instance.samples.Select(x =>
-                            {
-                                Random rm = new Random(x.name.GetHashCode());
-                                return new LineSeries
-                                {
-                                    Title = x.name,
-                                    Values = new ChartValues<ScatterPoint>(
-                                        x.points.Select(y => new ScatterPoint(y.X, y.Y))),
-                                    Fill = Brushes.Transparent,
-                                    Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
-                                    LineSmoothness = 1,
-                                    PointGeometrySize = 0
-                                };
-                            })
-                            .ToArray());
+                        LoadTrend2();
                         break;
 
                     case "태그 변동 - 한국어":
@@ -195,24 +177,6 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 1125000;
                         AxisY.Title = "작품 변동 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.AddRange(
-                            HitomiAnalysisTrend.Instance.samples.Select(x =>
-                            {
-                                Random rm = new Random(x.name.GetHashCode());
-                                return new LineSeries
-                                {
-                                    Title = x.name,
-                                    Values = new ChartValues<ScatterPoint>(
-                                        x.points.Select(y => new ScatterPoint(y.X, y.Y))),
-                                    Fill = Brushes.Transparent,
-                                    Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
-                                    LineSmoothness = 1,
-                                    PointGeometrySize = 0
-                                };
-                            })
-                            .ToArray());
                         break;
 
                     case "작가 작품 수 누적":
@@ -220,24 +184,7 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 0;
                         AxisY.Title = "누적 작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.AddRange(
-                            HitomiAnalysisTrend.Instance.samples.Select(x =>
-                            {
-                                Random rm = new Random(x.name.GetHashCode());
-                                return new LineSeries
-                                {
-                                    Title = x.name,
-                                    Values = new ChartValues<ScatterPoint>(
-                                        x.points.Select(y => new ScatterPoint(y.X, y.Y))),
-                                    Fill = Brushes.Transparent,
-                                    Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
-                                    LineSmoothness = 1,
-                                    PointGeometrySize = 0
-                                };
-                            })
-                            .ToArray());
+                        LoadTrend2();
                         break;
 
                     case "작가 작품 수 누적 - 한국어":
@@ -245,33 +192,59 @@ namespace Koromo_Copy_UX3.Tools
                         AxisX.Title = "아이디 간격";
                         AxisX.MinValue = 0;
                         AxisY.Title = "누적 작품 수";
-                        YSeparator.Step = HitomiAnalysisTrend.Instance.samples[0].points.Last().Y / 20;
-                        SeriesCollection.Clear();
-                        SeriesCollection.AddRange(
-                            HitomiAnalysisTrend.Instance.samples.Select(x =>
-                            {
-                                Random rm = new Random(x.name.GetHashCode());
-                                return new LineSeries
-                                {
-                                    Title = x.name,
-                                    Values = new ChartValues<ScatterPoint>(
-                                        x.points.Select(y => new ScatterPoint(y.X, y.Y))),
-                                    Fill = Brushes.Transparent,
-                                    Stroke = new SolidColorBrush(Color.FromRgb((byte)rm.Next(256), (byte)rm.Next(256), (byte)rm.Next(256))),
-                                    LineSmoothness = 1,
-                                    PointGeometrySize = 0
-                                };
-                            })
-                            .ToArray());
+                        LoadTrend2();
                         break;
 
                     case "작가 작품 수 변동 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateArtistsKoreanVariation();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 1125000;
+                        AxisY.Title = "작품 변동 수";
+                        break;
+
                     case "그룹 누적 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateGroupsKoreanIncremetns();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 0;
+                        AxisY.Title = "누적 작품 수";
+                        LoadTrend2();
+                        break;
+
                     case "그룹 변동 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateGroupsKoreanVariation();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 1125000;
+                        AxisY.Title = "작품 변동 수";
+                        break;
+
                     case "시리즈 누적 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateSeriesKoreanIncremetns();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 0;
+                        AxisY.Title = "누적 작품 수";
+                        LoadTrend2();
+                        break;
+
                     case "시리즈 변동 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateSeriesKoreanVariation();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 1125000;
+                        AxisY.Title = "작품 변동 수";
+                        break;
+
                     case "캐릭터 누적 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateCharactersKoreanIncremetns();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 0;
+                        AxisY.Title = "누적 작품 수";
+                        LoadTrend2();
+                        break;
+
                     case "캐릭터 변동 - 한국어":
+                        HitomiAnalysisTrend.Instance.UpdateCharactersKoreanVariation();
+                        AxisX.Title = "아이디 간격";
+                        AxisX.MinValue = 1125000;
+                        AxisY.Title = "작품 변동 수";
                         break;
                 }
             }
