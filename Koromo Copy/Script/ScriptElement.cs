dc836b251465e171e1249c7faa7ec60d8cffa8fa
dc-koromo <koromo.software@gmail.com>
2018-11-28 09:11:43 +0900

    script: add script engine prototype

---
+++ Koromo Copy/Script/ScriptElement.cs
@@ -0,0 +1,28 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Script
+{
+    public class ScriptElement
+    {
+        public bool UsingRegex;
+        public bool UsingXPath;
+        public string Pattern;
+
+        public string[] Parse(string html)
+        {
+            return null;
+        }
+    }
+}
