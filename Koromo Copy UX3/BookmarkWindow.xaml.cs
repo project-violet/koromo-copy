0bbcc5b4fdf26e796c7d48d673001ebd865567d3
dc-koromo <koromo.software@gmail.com>
2018-12-06 10:25:23 +0900

    ux: add Bookmark prototype

---
+++ Koromo Copy UX3/BookmarkWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3
+{
+    /// <summary>
+    /// BookmarkWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class BookmarkWindow : Window
+    {
+        public BookmarkWindow(Window owner)
+        {
+            InitializeComponent();
+
+            Owner = owner;
+        }
+    }
+}
