b592b6f721f3bbc0855ff835e4399e604a9efbf6
dc-koromo <koromo.software@gmail.com>
2018-12-10 10:49:23 +0900

    hitomi: Update date

--- Koromo Copy/Component/Hitomi/HitomiDate.cs
+++ Koromo Copy/Component/Hitomi/HitomiDate.cs
@@ -21,6 +21,7 @@ namespace Koromo_Copy.Component.Hitomi
     {
         public static Tuple<string, DateTime>[] date_data =
         {
+            new Tuple<string, DateTime> ("1327007", new DateTime(636800286600000000)),
             new Tuple<string, DateTime> ("1321257", new DateTime(636790276200000000)),
             new Tuple<string, DateTime> ("1318876", new DateTime(636786394200000000)),
             new Tuple<string, DateTime> ("1317006", new DateTime(636782575800000000)),
