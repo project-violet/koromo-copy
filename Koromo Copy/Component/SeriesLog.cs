df63b45439a05ae4de12041285a18e0d898c4f99
dc-koromo <koromo.software@gmail.com>
2018-12-10 10:48:38 +0900

    kcp: add series logger prototype

---
+++ Koromo Copy/Component/SeriesLog.cs
@@ -0,0 +1,22 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Component.Hiyobi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Component
+{
+    public class SeriesLog
+    {
+        List<HiyobiArticle> HiyobiLogs;
+    }
+}
