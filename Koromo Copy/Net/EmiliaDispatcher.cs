ddf5013c042b9d07b05acddaa85c267467b370de
dc-koromo <koromo.software@gmail.com>
2018-12-12 15:22:53 +0900

    net: add EmiliaDispatcher prototype

---
+++ Koromo Copy/Net/EmiliaDispatcher.cs
@@ -0,0 +1,31 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Net
+{
+    /// <summary>
+    /// 단일 시리즈에 대한 다운로드 관리를 지원하는 다운로드 큐 관리자 입니다.
+    /// </summary>
+    public class EmiliaDispatcher
+    {
+        ISemaphore queue;
+
+        #region Events
+
+
+
+        #endregion
+    }
+}
