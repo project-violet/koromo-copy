7b807ea0a458e025c243515f21fcb64b2e4c4944
dc-koromo <koromo.software@gmail.com>
2018-12-16 00:23:33 +0900

    ux/kcp: Implements some part of IManager structure

--- Koromo Copy UX3/Utility/SeriesManager.xaml.cs
+++ Koromo Copy UX3/Utility/SeriesManager.xaml.cs
@@ -29,6 +29,11 @@ namespace Koromo_Copy_UX3.Utility
         private void SeriesManager_Loaded(object sender, RoutedEventArgs e)
         {
             SeriesPanel.Children.Add(new SeriesManagerElements("https://mangashow.me/bbs/page.php?hid=manga_detail&manga_name=%EC%84%B8%EA%B3%84+%EC%A2%85%EC%96%B8%EC%9D%98+%EC%84%B8%EA%B3%84%EB%A1%9D"));
+            SeriesPanel.Children.Add(new Separator());
+            SeriesPanel.Children.Add(new SeriesManagerElements("https://mangashow.me/bbs/page.php?hid=manga_detail&manga_name=%EC%BF%A0%EB%A1%9C%ED%95%98%EC%99%80%20%EB%8B%88%EC%A7%80%EC%8A%A4%EC%BC%80"));
+            SeriesPanel.Children.Add(new Separator());
+            SeriesPanel.Children.Add(new SeriesManagerElements("https://mangashow.me/bbs/page.php?hid=manga_detail&manga_name=%EB%9F%AC%EC%8A%A4%ED%8A%B8%20%EA%B8%B0%EC%95%84%EC%8A%A4"));
+            SeriesPanel.Children.Add(new Separator());
         }
     }
 }
--- Koromo Copy UX3/Utility/SeriesManagerElements.xaml.cs
+++ Koromo Copy UX3/Utility/SeriesManagerElements.xaml.cs
@@ -12,6 +12,7 @@ using Koromo_Copy.Interface;
 using Koromo_Copy.Net;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,14 +34,22 @@ namespace Koromo_Copy_UX3.Utility
     public partial class SeriesManagerElements : UserControl
     {
         string url;
-        SeriesInfo series_info;
         IManager manager;
+        ISeries series;
+        IArticle article;
+
+        public SeriesManagerElements(SeriesInfo info)
+        {
+            InitializeComponent();
+
+        }
 
         public SeriesManagerElements(string url)
         {
             InitializeComponent();
 
             this.url = url;
+            Title.Text = url;
             Loaded += SeriesManagerElements_Loaded;
         }
 
@@ -48,25 +57,96 @@ namespace Koromo_Copy_UX3.Utility
         {
             Task.Run(() =>
             {
-                series_info = new SeriesInfo(url);
-                manager = MangashowmeManager.Instance;
-
-                ISeries article = manager.ParseSeries(NetCommon.DownloadString(url));
-
-                Application.Current.Dispatcher.BeginInvoke(new Action(
-                delegate
+                if (!string.IsNullOrEmpty(url))
                 {
-                    var bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.UriSource = new Uri(article.Thumbnail);
-                    //bitmap.DecodePixelWidth = 250;
-                    //bitmap.CacheOption = BitmapCacheOption.OnLoad;
-                    bitmap.EndInit();
-                    Image.Source = bitmap;
+                    manager = SeriesInfo.Instance.SelectManager(url);
 
-                    Title.Text = article.Title;
-                }));
+                    string title = "";
+                    string thumbnail = "";
+
+                    var wc = manager.GetWebClient();
+
+                    string top_html = "";
+
+                    if (wc != null)
+                        top_html = wc.DownloadString(url);
+                    else
+                        top_html = NetCommon.DownloadString(url);
+
+                    string inner_counts = "";
+
+                    switch (manager.Type)
+                    {
+                        case ManagerType.SingleArticleSingleImage:
+                            {
+
+                            }
+                            break;
+
+                        case ManagerType.SingleArticleSingleMovie:
+                            {
+
+                            }
+                            break;
+
+                        case ManagerType.SingleArticleMultipleImages:
+                            {
+                                article = manager.ParseArticle(top_html);
+                                title = article.Title;
+                                thumbnail = article.Thumbnail;
+                                inner_counts = $"사진 {article.ImagesLink.Count}장";
+                            }
+                            break;
+
+                        case ManagerType.SingleSeriesMultipleArticles:
+                            {
+                                series = manager.ParseSeries(top_html);
+                                title = series.Title;
+                                thumbnail = series.Thumbnail;
+                                inner_counts = $"작품 {series.Articles.Count}개";
+                            }
+                            break;
+                    }
+
+                    Application.Current.Dispatcher.BeginInvoke(new Action(
+                    delegate
+                    {
+                        if (!string.IsNullOrEmpty(thumbnail))
+                        {
+                            var bitmap = new BitmapImage();
+                            bitmap.BeginInit();
+                            bitmap.UriSource = new Uri(thumbnail);
+                            //bitmap.DecodePixelHeight = 160;
+                            bitmap.EndInit();
+                            Image.Source = bitmap;
+                        }
+
+                        Title.Text = title;
+                        SiteName.Text = manager.Name;
+                        LatestSyncDate.Text = MakeSyncDate(new TimeSpan(0, 1, 0, 0));
+                        InnerSitesCount.Text = inner_counts;
+                    }));
+                    
+                    Task.Run(() => StartFirstDownloads());
+                }
             });
+        }
+
+        private void StartFirstDownloads()
+        {
+
+        }
+
+        private string MakeSyncDate(TimeSpan gap)
+        {
+            if (gap.Days >= 730) return "2년 전";
+            if (gap.Days >= 365) return "1년 전";
+            if (gap.Days >= 30) return $"{gap.Days / 30} 개월 전";
+            if (gap.Days >= 2) return $"{gap.Days}일 전";
+            if (gap.Days >= 1) return $"하루 전";
+            if (gap.Hours >= 1) return $"{gap.Hours}시간 전";
+            if (gap.Minutes >= 1) return $"{gap.Minutes}분 전";
+            return "방금";
         }
         
         private void Image_MouseMove(object sender, MouseEventArgs e)
@@ -74,6 +154,21 @@ namespace Koromo_Copy_UX3.Utility
             ImageToolTip.Placement = System.Windows.Controls.Primitives.PlacementMode.Relative;
             ImageToolTip.HorizontalOffset = e.GetPosition((IInputElement)sender).X + 10;
             ImageToolTip.VerticalOffset = e.GetPosition((IInputElement)sender).Y;
+        }
+
+        private void SiteName_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SiteName.Foreground = Brushes.White;
+        }
+
+        private void SiteName_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SiteName.Foreground = new SolidColorBrush(Color.FromRgb(0xAA, 0xAA, 0xAA));
+        }
+
+        private void SiteName_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(url);
         }
     }
 }
