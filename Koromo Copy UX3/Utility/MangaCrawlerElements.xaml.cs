e7e2384a90c51fce3b1365642c62375d23f7253a
dc-koromo <koromo.software@gmail.com>
2018-12-20 20:31:34 +0900

    ux: Implements manga crawler elements

---
+++ Koromo Copy UX3/Utility/MangaCrawlerElements.xaml.cs
@@ -0,0 +1,66 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    /// <summary>
+    /// MangaCrawlerElements.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MangaCrawlerElements : UserControl
+    {
+        string image;
+        string url;
+        public MangaCrawlerElements(string image, string url, string title)
+        {
+            InitializeComponent();
+
+            this.image = image;
+            this.url = url;
+            Loaded += MangaCrawlerElements_Loaded;
+            Title.Text = title;
+        }
+
+        private void MangaCrawlerElements_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Application.Current.Dispatcher.BeginInvoke(new Action(
+                delegate
+                {
+                    var bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(image);
+                    bitmap.EndInit();
+
+                    Image.Source = bitmap;
+                }));
+            });
+        }
+
+        private void UserControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start($"https://mangashow.me{url}");
+        }
+    }
+}
