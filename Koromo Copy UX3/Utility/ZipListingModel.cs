c24216d022c2523d3a7bd9d02c4551a1864a4918
dc-koromo <koromo.software@gmail.com>
2018-12-28 16:46:30 +0900

    ux: implements zip-listing

---
+++ Koromo Copy UX3/Utility/ZipListingModel.cs
@@ -0,0 +1,68 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy;
+using Koromo_Copy.Component.Hitomi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    /// <summary>
+    /// Zip Listing Tool Data Model
+    /// </summary>
+    public class ZipListingModel
+    {
+        /// <summary>
+        /// 최초로 열거작업을 수행한 폴더입니다. 루트 경로를 찾을 수 없다면 대화형으로 경로를 찾습니다.
+        /// </summary>
+        [JsonProperty]
+        public string RootDirectory;
+
+        /// <summary>
+        /// 사용자가 지정한 태그입니다.
+        /// </summary>
+        [JsonProperty]
+        public string Tag;
+
+        /// <summary>
+        /// 튜플의 첫 번째 요소는 해당 파일의 상대경로이며, 두 번째 요소는 작품의 정보입니다.
+        /// </summary>
+        [JsonProperty]
+        public KeyValuePair<string, HitomiJsonModel>[] ArticleList;
+    }
+
+    public class ZipListingModelManager
+    {
+        public static void SaveModel(string filename, ZipListingModel model)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+
+            Monitor.Instance.Push($"Write file: {filename}");
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filename)))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, model);
+            }
+        }
+
+        public static ZipListingModel OpenModel(string filename)
+        {
+            return JsonConvert.DeserializeObject<ZipListingModel>(File.ReadAllText(filename));
+        }
+    }
+}
