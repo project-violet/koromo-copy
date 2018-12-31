103ed15c7e541c4b5cc7363fee9f4e4b7a65b9c2
dc-koromo <koromo.software@gmail.com>
2018-12-31 15:33:33 +0900

    kcp: implements stringtools sorting

---
+++ Koromo Copy/Utility/UtilityDelegator.cs
@@ -0,0 +1,34 @@
+﻿/***
+
+   Copyright (C) 2018. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using Hitomi_Copy_3;
+
+namespace Koromo_Copy.Utility
+{
+    public class UtilityDelegator
+    {
+        public static void Run(string toch)
+        {
+            switch (toch)
+            {
+                case "HitomiExplorer":
+                    (new HitomiExplorer()).Show();
+                    break;
+                case "FsEnumerator":
+                    (new FsEnumerator()).Show();
+                    break;
+                case "RelatedTagsTest":
+                    (new RelatedTagsTest()).Show();
+                    break;
+                case "StringTools":
+                    (new StringTools()).Show();
+                    break;
+            }
+        }
+    }
+}
