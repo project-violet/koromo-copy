365427b95aee3dcf7fe9599fa891ea95d33f20bf
dc-koromo <koromo.software@gmail.com>
2019-01-07 20:52:40 +0900

    kcp: add hcommander prototype

---
+++ Koromo Copy/Component/HCommander.cs
@@ -0,0 +1,23 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Component
+{
+    /// <summary>
+    /// E Hentai 미러 웹 사이트들에 대한 지원을 제공합니다.
+    /// </summary>
+    public class HCommander
+    {
+    }
+}
