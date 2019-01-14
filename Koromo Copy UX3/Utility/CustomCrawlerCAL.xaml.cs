a1b02ee0ab9ebc2bf74a1abf8d98e8ea7784d00c
dc-koromo <koromo.software@gmail.com>
2019-01-14 10:09:32 +0900

    html/ux: Implements HtmlCAL in custom crawler

---
+++ Koromo Copy UX3/Utility/CustomCrawlerCAL.xaml.cs
@@ -0,0 +1,53 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using HtmlAgilityPack;
+using Koromo_Copy.Html;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    /// <summary>
+    /// CustomCrawlerCAL.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class CustomCrawlerCAL : Window
+    {
+        HtmlNode root_node;
+
+        public CustomCrawlerCAL(HtmlNode root_node)
+        {
+            InitializeComponent();
+
+            this.root_node = root_node;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                Result.Text = string.Join("\r\n", HtmlCAL.Calculate(Pattern.Text, root_node));
+            }
+            catch (Exception ex)
+            {
+                Result.Text = ex.Message + "\r\n" + ex.StackTrace;
+            }
+        }
+    }
+}
---
+++ Koromo Copy/Html/HtmlCAL.cs
@@ -0,0 +1,143 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Html
+{
+    public class HtmlCAL
+    {
+        private static List<string> split_token(string str)
+        {
+            var result = new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                var builder = new StringBuilder();
+                bool skip = false;
+                for (; i < str.Length; i++)
+                {
+                    if (str[i] == ',' && skip == false)
+                    {
+                        result.Add(builder.ToString());
+                        break;
+                    }
+                    if (str[i] == '[')
+                        skip = true;
+                    else if (str[i] == ']' && skip)
+                        skip = false;
+                    builder.Append(str[i]);
+                }
+                if (i == str.Length)
+                    result.Add(builder.ToString());
+            }
+            return result;
+        }
+
+        public static List<string> Calculate(string pattern, HtmlNode root_node)
+        {
+            var result = new List<string>();
+            var split = split_token(pattern);
+            var xpath = split[0].Trim();
+            var extend = split.Count > 0 ? split[1].Trim() : "";
+            int ptr = 1;
+
+            var explore = root_node.SelectSingleNode(xpath);
+
+            if (string.IsNullOrEmpty(extend) || extend == "#text")
+                result.Add(explore.InnerText);
+            else if (extend == "#html")
+                result.Add(explore.InnerHtml);
+            else if (extend == "#ohtml")
+                result.Add(explore.OuterHtml);
+            else if (extend == "#attr")
+            {
+                var val = extend.Split('[')[1].Split(']')[0].Trim();
+                result.Add(explore.GetAttributeValue(val, ""));
+            }
+
+            if (result.Count > 0)
+                ptr++;
+
+            if (extend.StartsWith("."))
+            {
+                var nodes = explore.SelectNodes(extend);
+                ptr++;
+                if (split.Count >= 3)
+                {
+                    extend = split[2].Trim();
+                    if (extend.StartsWith("#attr"))
+                    {
+                        // #attr[src] ...
+                        var val = extend.Split('[')[1].Split(']')[0].Trim();
+                        nodes.ToList().ForEach(x => result.Add(x.GetAttributeValue(val, "")));
+                    }
+                    else if (extend == "#text")
+                        nodes.ToList().ForEach(x => result.Add(x.InnerText));
+                    else if (extend == "#html")
+                        nodes.ToList().ForEach(x => result.Add(x.InnerHtml));
+                    else if (extend == "#ohtml")
+                        nodes.ToList().ForEach(x => result.Add(x.OuterHtml));
+                    ptr++;
+                }
+                else
+                {
+                    nodes.ToList().ForEach(x => result.Add(x.InnerText));
+                }
+            }
+
+            while (ptr != split.Count)
+            {
+                var token = split[ptr].Trim();
+
+                if (token.StartsWith("#split"))
+                {
+                    // #split[text, index]
+                    var val = token.Split(new char[] { '[' }, 2)[1];
+                    val = val.Remove(val.LastIndexOf(']'));
+                    var text = val.Remove(val.LastIndexOf(',')).Trim();
+                    int index = Convert.ToInt32(val.Split(',').Last().Trim());
+                    var tmp = new List<string>();
+
+                    if (index != -1)
+                        result.ForEach(x => tmp.Add(x.Split(new string[] { text }, StringSplitOptions.None)[index]));
+                    else
+                        result.ForEach(x => tmp.Add(x.Split(new string[] { text }, StringSplitOptions.None).Last()));
+                    result = tmp;
+                }
+                else if (token.StartsWith("#regex"))
+                {
+                    // #regex[pattern]
+                    var val = token.Split(new char[] { '[' }, 2)[1];
+                    val = val.Remove(val.LastIndexOf(']'));
+                    var regex = new Regex(val);
+                    var tmp = new List<string>();
+                    result.ForEach(x => tmp.Add(regex.Match(x).Value));
+                }
+                else if (token.StartsWith("#gregex"))
+                {
+                    // #gregex[pattern]
+                    var val = token.Split(new char[] { '[' }, 2)[1];
+                    val = val.Remove(val.LastIndexOf(']'));
+                    var regex = new Regex(val);
+                    var tmp = new List<string>();
+                    result.ForEach(x => tmp.Add(regex.Match(x).Groups[1].Value));
+                }
+
+                ptr++;
+            }
+
+            return result;
+        }
+    }
+}
