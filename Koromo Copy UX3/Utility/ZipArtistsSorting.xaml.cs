16187d5243ea78c6e9cba7933e97464032e77a5f
dc-koromo <koromo.software@gmail.com>
2019-02-19 22:02:25 +0900

    ux: implements zip-artists

---
+++ Koromo Copy UX3/Utility/ZipArtistsSorting.xaml.cs
@@ -0,0 +1,51 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    /// <summary>
+    /// ZipArtistsSorting.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ZipArtistsSorting : UserControl
+    {
+        public ZipArtistsSorting(int c, int r)
+        {
+            InitializeComponent();
+
+            AlignColumn.SelectedIndex = c;
+            AlignRow.SelectedIndex = r;
+        }
+
+        public int AlignColumnIndex = 0;
+        private void AlignColumn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AlignColumnIndex = AlignColumn.SelectedIndex;
+        }
+
+        public int AlignRowIndex = 0;
+        private void AlignRow_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AlignRowIndex = AlignRow.SelectedIndex;
+        }
+    }
+}
