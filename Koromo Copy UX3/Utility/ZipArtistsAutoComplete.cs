18da552ec0d5c383c4d039b9239539b875efcae0
dc-koromo <koromo.software@gmail.com>
2019-02-20 15:00:15 +0900

    ux: implements search in zip-artists

---
+++ Koromo Copy UX3/Utility/ZipArtistsAutoComplete.cs
@@ -0,0 +1,83 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using Koromo_Copy.Component.Hitomi;
+using Koromo_Copy_UX3.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    public class ZipArtistsAutoComplete : IAutoCompleteAlgorithm
+    {
+        HitomiTagdataCollection tagdata_collection;
+        bool builded = false;
+
+        private void Add(Dictionary<string, int> dic, string key)
+        {
+            if (dic.ContainsKey(key))
+                dic[key] += 1;
+            else
+                dic.Add(key, 1);
+        }
+
+        public void Build(ZipArtistsModel src)
+        {
+            tagdata_collection = new HitomiTagdataCollection();
+            tagdata_collection.artist?.Clear();
+
+            Dictionary<string, int> artist = new Dictionary<string, int>();
+            
+            src.ArtistList.Select(x => x.Value).Select(x => x.ArticleData.Select(y => y.Value)).ToList().ForEach(x => x.ToList().ForEach(article =>
+            {
+                if (article.Artists != null) article.Artists.ToList().ForEach(z => Add(artist, z));
+            }));
+
+            tagdata_collection.artist = artist.Select(x => new HitomiTagdata() { Tag = x.Key, Count = x.Value }).ToList();
+            tagdata_collection.artist.Sort((a, b) => b.Count.CompareTo(a.Count));
+            builded = true;
+        }
+
+        public List<HitomiTagdata> GetResults(ref string word, ref int position)
+        {
+            var match = new List<HitomiTagdata>();
+
+            if (!builded) return match;
+
+            var old_data = HitomiData.Instance.tagdata_collection;
+            HitomiData.Instance.tagdata_collection = tagdata_collection;
+
+            if (word.Contains(":"))
+            {
+                if (word.StartsWith("artist:"))
+                {
+                    word = word.Substring("artist:".Length);
+                    position += "artist:".Length;
+                    match = HitomiDataAnalysis.GetArtistList(word);
+                }
+            }
+
+            string[] match_target = {
+                    "artist:",
+                };
+
+            string w = word;
+            List<HitomiTagdata> data_col = (from ix in match_target where ix.StartsWith(w) select new HitomiTagdata { Tag = ix }).ToList();
+            if (data_col.Count > 0)
+                match.AddRange(data_col);
+            match.AddRange(HitomiDataAnalysis.GetArtistList(word));
+
+            HitomiData.Instance.tagdata_collection = old_data;
+
+            return match;
+        }
+    }
+}
---
+++ Koromo Copy UX3/Utility/ZipArtistsFilter.xaml.cs
@@ -0,0 +1,62 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX3.Utility
+{
+    /// <summary>
+    /// ZipArtistsFilter.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ZipArtistsFilter : UserControl
+    {
+        public ZipArtistsFilter(List<DateTime> dates, DateTime? starts, DateTime? ends, bool show_bookmark)
+        {
+            InitializeComponent();
+
+            if (dates.Count > 0)
+            {
+                StartDate.DisplayDateStart = dates.Min();
+                StartDate.DisplayDate = dates.Min();
+                StartDate.DisplayDateEnd = dates.Max();
+
+                if (starts.HasValue) StartDate.SelectedDate = starts;
+
+                EndDate.DisplayDateStart = dates.Min();
+                EndDate.DisplayDateEnd = dates.Max();
+
+                if (ends.HasValue) EndDate.SelectedDate = ends;
+            }
+
+            ShowBookmark.IsChecked = show_bookmark;
+        }
+
+        private void StartDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EndDate.DisplayDateStart = StartDate.SelectedDate;
+        }
+
+        private void EndDate_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StartDate.DisplayDateEnd = EndDate.SelectedDate;
+        }
+    }
+}
