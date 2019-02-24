761a4de535ba12e790e9ab47a7cba58415b81efc
dc-koromo <koromo.software@gmail.com>
2019-02-24 22:17:48 +0900

    zip-artists: prototype transform

--- Koromo Copy UX/Utility/ZipArtists/ZipArtistsToolTransform.xaml.cs
+++ Koromo Copy UX/Utility/ZipArtists/ZipArtistsToolTransform.xaml.cs
@@ -6,6 +6,7 @@
 
 ***/
 
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,6 +32,21 @@ namespace Koromo_Copy_UX.Utility.ZipArtists
         public ZipArtistsToolTransform()
         {
             InitializeComponent();
+        }
+
+        private void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var tag = (sender as Button).Tag.ToString();
+            if (tag == "SelectFolder")
+            {
+                var cofd = new CommonOpenFileDialog();
+                cofd.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                cofd.IsFolderPicker = true;
+                if (cofd.ShowDialog(Window.GetWindow(this)) == CommonFileDialogResult.Ok)
+                {
+                    Folder.Text = cofd.FileName;
+                }
+            }
         }
     }
 }
