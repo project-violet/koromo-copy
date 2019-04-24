316b78523005b796faa1be18ae1f984f9af9479e
dc-koromo <koromo.software@gmail.com>
2019-04-24 10:09:16 +0900

    html: add html-diff tool prototype

---
+++ Koromo Copy/Html/HtmlDiff.cs
@@ -0,0 +1,39 @@
+﻿/***
+
+   Copyright (C) 2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Html
+{
+    public class HtmlDiff
+    {
+        public struct DiffURLResult
+        {
+
+        }
+
+        public static DiffURLResult DiffURL(string s1, string s2)
+        {
+            return new DiffURLResult();
+        }
+
+        public struct DiffHtmlResult
+        {
+
+        }
+
+        public static DiffHtmlResult DiffHtml(string s1, string s2)
+        {
+            return new DiffHtmlResult();
+        }
+    }
+}
