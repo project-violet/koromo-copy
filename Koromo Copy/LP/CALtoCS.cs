550a0591d92b9ca55192ce2fa23aa33ca6cc474c
dc-koromo <koromo.software@gmail.com>
2019-06-22 18:53:39 +0900

    lp: add cal-to-cs prototype

---
+++ Koromo Copy/LP/CALtoCS.cs
@@ -0,0 +1,36 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.LP
+{
+    /// <summary>
+    /// SRCAL문법을 C#문법으로 컴파일하는 도구집합입니다.
+    /// </summary>
+    public class CALtoCS
+    {
+        public CALtoCS(string cal)
+        {
+
+        }
+
+        /// <summary>
+        /// SRCAL문법을 C#으로 바꿉니다.
+        /// </summary>
+        /// <returns></returns>
+        public string Compile()
+        {
+            return "";
+        }
+    }
+}
