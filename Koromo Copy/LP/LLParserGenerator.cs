df39f415ea6230e8c7f266e0bccefcd64a6a3a1c
dc-koromo <koromo.software@gmail.com>
2019-07-04 21:35:53 +0900

    lp: add ll-parser-generator prototype

---
+++ Koromo Copy/LP/LLParserGenerator.cs
@@ -0,0 +1,34 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.LP
+{
+    public class LLParserProduction
+    {
+        // LL Parser allow L-Attributed.
+    }
+
+    public class LLParserGenerator
+    {
+        private bool detect_leftrecursion()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool eliminate_leftrecursion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
