dd30c90d1c03a117cf6b87ee68ddec718e7cbe60
dc-koromo <koromo.software@gmail.com>
2019-07-09 11:00:07 +0900

    net/algorithm: add task scheduler prototype

---
+++ Koromo Copy/Algorithm/Heap.cs
@@ -0,0 +1,50 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Algorithm
+{
+    public class Heap<T, C>
+        where T : IComparable
+        where C : IComparer
+    {
+        List<T> heap;
+
+        public Heap(int capacity = 256)
+        {
+            heap = new List<T>(capacity);
+        }
+
+        public void Push(T d)
+        {
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public T Front()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Heap<T> 
+        : Heap<T, Comparer<T>> 
+        where T : IComparable
+    {
+    }
+}
