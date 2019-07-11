e0dcb557a034f26fa1d6fafefc2cee7f9d2f3b69
dc-koromo <koromo.software@gmail.com>
2019-07-11 20:56:47 +0900

    lp: add bnf-parser prototype

---
+++ Koromo Copy/LP/BNFParser.cs
@@ -0,0 +1,23 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.LP
+{
+    /// <summary>
+    /// BNF to Parser Descriptions
+    /// </summary>
+    public class BNFParser
+    {
+    }
+}
