87842efafec06e18ba8ceb4642816cb3a0f8ada8
dc-koromo <koromo.software@gmail.com>
2019-07-13 12:51:52 +0900

    ux: implements dangling finder on bookmark tool

---
+++ Koromo Copy UX/Utility/Bookmark/BookmarkMessageOk.xaml.cs
@@ -0,0 +1,38 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Koromo_Copy_UX.Utility.Bookmark
+{
+    /// <summary>
+    /// BookmarkMessageOk.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class BookmarkMessageOk : UserControl
+    {
+        public BookmarkMessageOk(string msg)
+        {
+            InitializeComponent();
+
+            Message.Text = msg;
+        }
+    }
+}
