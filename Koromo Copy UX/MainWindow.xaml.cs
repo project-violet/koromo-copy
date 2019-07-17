0af0947567606af3fddf9e413eba62f7af42a239
Lulin Pollux <kistssy+dev@gmail.com>
2019-07-17 15:38:27 +0900

    ux: change event method name
    ThreeButton_Click → CaptionButton_Click

--- Koromo Copy UX/MainWindow.xaml.cs
+++ Koromo Copy UX/MainWindow.xaml.cs
@@ -251,7 +251,7 @@ namespace Koromo_Copy_UX
             DropShadow.Color = SettingWrap.Instance.ThemeColor;
         }
         
-        private void ThreeButton_Click(object sender, RoutedEventArgs e)
+        private void CaptionButton_Click(object sender, RoutedEventArgs e)
         {
             var tag = (sender as Button).Tag.ToString();
             if (tag == "Minimize")
