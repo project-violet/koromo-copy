87646b25c3d5ad56b5650525cd814567d64e3bda
dc-koromo <koromo.software@gmail.com>
2019-07-18 12:24:53 +0900

    script/utlity: add auto-console and koromo-script base

---
+++ Koromo Copy/Script/Koromo/KoromoCopyScript.cs
@@ -0,0 +1,23 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+   
+   Author: Koromo Copy Developer
+
+***/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Script.Koromo
+{
+    /// <summary>
+    /// 코로모 카피의 동작을 제어할 수 있는 스크립트 엔진입니다.
+    /// </summary>
+    public class KoromoCopyScript
+    {
+    }
+}
