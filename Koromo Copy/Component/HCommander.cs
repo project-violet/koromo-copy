ed348cda6cf91d94c8e59e222cb0643a5f84a624
dc-koromo <koromo.software@gmail.com>
2019-07-27 22:43:08 +0900

    hitomi: remove ex-hentai handler

--- Koromo Copy/Component/HCommander.cs
+++ Koromo Copy/Component/HCommander.cs
@@ -110,86 +110,24 @@ namespace Koromo_Copy.Component
                 }
             }
 
-            //Monitor.Instance.Push($"[HCommander] [2] {magic}");
+            Monitor.Instance.Push($"[HCommander] [2] {magic}");
 
             //
             // 2. Hiyobi를 이용해 탐색한다
             //
-            //if (search_hitomi.HasValue && search_hitomi.Value.Language == "korean")
-            //{
-            //    try
-            //    {
-            //        var html = NetCommon.DownloadString(HiyobiCommon.GetInfoAddress(magic));
-            //        var article = HiyobiParser.ParseGalleryConents(html);
-            //        return ConvertTo(article, HiyobiCommon.GetInfoAddress(magic), magic);
-            //    }
-            //    catch
-            //    {
-            //    }
-            //}
-
-            Monitor.Instance.Push($"[HCommander] [3] {magic}");
-            
-            //
-            // 9.3/4 샰쮘뽣?뛤3쇼뵀?gVA덲탭k융뷠킢쪳1SPS?XF퍵8C샜쁬
-            //
-            var f = ExHentaiData.data.AsParallel().Where(x => (x >> 40) == magic_number).ToList();
-            if (f.Count > 0)
+            if (search_hitomi.HasValue && HitomiIndex.Instance.index.Languages[search_hitomi.Value.Language] == "korean")
             {
                 try
                 {
-                    //var url = $"https://e-hentai.org/g/{magic}/{f[0] ^ 1L * magic_number << 40:x}/";
-                    //var html2 = NetCommon.DownloadExHentaiString(url);
-                    //var article = EHentaiParser.ParseArticleData(html2);
-                    //return ConvertTo(article, url, magic);
-                    throw new Exception();
+                    var html = NetCommon.DownloadString(HiyobiCommon.GetInfoAddress(magic));
+                    var article = HiyobiParser.ParseGalleryConents(html);
+                    return ConvertTo(article, HiyobiCommon.GetInfoAddress(magic), magic);
                 }
                 catch
                 {
-                    var x = $"{f[0] ^ 1L * magic_number << 40:x}";
-                    if (x.Length == "787da3694".Length) x = "0" + x;
-                    var url = $"https://exhentai.org/g/{magic}/{x}/";
-                    var html2 = NetCommon.DownloadExHentaiString(url);
-                    var article = ExHentaiParser.ParseArticleData(html2);
-                    return ConvertTo(article, url, magic);
                 }
             }
 
-            //
-            // 3. GalleryBlock을 이용해 제목을 가져온다.
-            //
-            string title = "";
-            try
-            {
-                var html = NetCommon.DownloadString($"{HitomiCommon.HitomiGalleryBlock}{magic}.html");
-                var article = HitomiParser.ParseGalleryBlock(html);
-                title = article.Title;
-            }
-            catch
-            {
-                Monitor.Instance.Push($"[HCommander] [0] {magic}");
-                return null;
-            }
-
-            //
-            // 4. 'Show Expunged Galleries' 옵션을 이용해 Ex-Hentai에서 검색한 후 정보를 가져온다.
-            //
-            try
-            {
-                var html = NetCommon.DownloadExHentaiString($"https://exhentai.org/?f_doujinshi=1&f_manga=1&f_artistcg=1&f_gamecg=1&f_western=1&f_non-h=1&f_imageset=1&f_cosplay=1&f_asianporn=1&f_misc=1&f_search={title}&page=0&f_apply=Apply+Filter&advsearch=1&f_sname=on&f_stags=on&f_sh=on&f_srdd=2");
-
-                if (html.Contains($"/{magic}/"))
-                {
-                    var url = Regex.Match(html, $"(https://exhentai.org/g/{magic}/\\w+/)").Value;
-                    var html2 = NetCommon.DownloadExHentaiString(url);
-                    var article = ExHentaiParser.ParseArticleData(html2);
-                    return ConvertTo(article, url, magic);
-                }
-            }
-            catch
-            { }
-
-            Monitor.Instance.Push($"[HCommander] [0] {magic}");
             return null;
         }
         
@@ -225,10 +163,22 @@ namespace Koromo_Copy.Component
             return article;
         }
 
-        /*private static HArticleModel ConvertTo(HitomiArticle article, string url, string magic)
+        private static HArticleModel ConvertTo(HitomiArticle article, string url, string magic)
         {
-            return ConvertTo(HitomiLegalize.ArticleToMetadata(article), url, magic);
-        }*/
+            var article2 = new HArticleModel();
+            article2.Magic = magic;
+            article2.ArticleType = HArticleType.Hitomi;
+            article2.URL = url;
+            article2.artist = article.Artists;
+            article2.group = article.Groups;
+            article2.parody = article.Series;
+            article2.misc = article.Tags;
+            article2.character = article.Characters;
+            article2.Language = article.Language;
+            article2.Title = article.Title;
+            article2.Type = article.Type;
+            return article2;
+        }
 
         private static HArticleModel ConvertTo(HiyobiArticle data, string url, string magic)
         {
