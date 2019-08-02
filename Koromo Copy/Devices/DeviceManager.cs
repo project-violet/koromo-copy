a6dbcfaa5e0ec53b210fd7b17ea4935f387c559f
dc-koromo <koromo.software@gmail.com>
2019-08-02 18:35:37 +0900

    device: add device-manager prototype

---
+++ Koromo Copy/Devices/DeviceManager.cs
@@ -0,0 +1,25 @@
+﻿/***
+
+   Copyright (C) 2018-2019. dc-koromo. All Rights Reserved.
+
+   Author: Koromo Copy Developer
+
+***/
+
+using MediaDevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koromo_Copy.Devices
+{
+    public class DeviceManager
+    {
+        public static void EnumerateDevice()
+        {
+            var devices = MediaDevice.GetDevices();
+        }
+    }
+}
