e84609e95dcf01a7b74b626988591752331d69ad
dc-koromo <koromo.software@gmail.com>
2019-08-03 20:57:37 +0900

    lp: implements some code

--- Koromo Copy/LP/Code/LPModule.cs
+++ Koromo Copy/LP/Code/LPModule.cs
@@ -21,11 +21,27 @@ namespace Koromo_Copy.LP.Code
     {
         List<LPFunction> funcs;
 
-        public LPFunction CreateFunction()
+        public LPModule()
         {
-            var func = new LPFunction(this);
+            funcs = new List<LPFunction>();
+        }
+
+        public LPFunction CreateFunction(string name, LPType return_type, List<LPArgument> args)
+        {
+            if (funcs.Any(x => x.Name == name))
+                return funcs.Where(x => x.Name == name).ElementAt(0);
+            var func = new LPFunction(this, name, return_type, args);
             funcs.Add(func);
-            return new LPFunction(this);
+            return func;
+        }
+
+        public LPFunction CreateFunction(string name)
+        {
+            if (funcs.Any(x => x.Name == name))
+                return funcs.Where(x => x.Name == name).ElementAt(0);
+            var func = new LPFunction(this, name, new LPType { Type = LPType.TypeOption.t_void }, new List<LPArgument>());
+            funcs.Add(func);
+            return func;
         }
     }
 }
