0ddd68469e699a7e85b5276819526f05f4a5abcc
koromo <koromo.software@gmail.com>
2019-10-30 09:02:41 +0900

    Update HitomiParser.cs

--- Koromo Copy/Component/Hitomi/HitomiParser.cs
+++ Koromo Copy/Component/Hitomi/HitomiParser.cs
@@ -28,7 +28,7 @@ namespace Koromo_Copy.Component.Hitomi
             document.LoadHtml(source);
             HtmlNode nodes = document.DocumentNode.SelectNodes("/div")[0];
 
-            article.Magic = nodes.SelectSingleNode("./a").GetAttributeValue("href", "").Split('/')[2].Split('.')[0];
+            article.Magic = nodes.SelectSingleNode("./a").GetAttributeValue("href", "");
             try { article.Thumbnail = nodes.SelectSingleNode("./a//img").GetAttributeValue("data-src", "").Substring("//tn.hitomi.la/".Length).Replace("smallbig", "big"); } catch
                 { article.Thumbnail = nodes.SelectSingleNode("./a//img").GetAttributeValue("src", "").Substring("//tn.hitomi.la/".Length); }
             article.Title = nodes.SelectSingleNode("./h1").InnerText;
